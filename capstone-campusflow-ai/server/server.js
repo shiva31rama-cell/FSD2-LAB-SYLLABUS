87afc837ec168ff53f96009ae8a3a7c68f6dd2e6
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 01:17:52 +0530

    fix(capstone): support current connect-mongo export

--- capstone-campusflow-ai/server/server.js
+++ capstone-campusflow-ai/server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 
